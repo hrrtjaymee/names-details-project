2ca5111d8d6908210a8a5d8f53f2334d46cf8815
hrrtjaymee <harrietregencia@gmail.com>
2026-05-20 15:02:21 +0800

    feat: initialized testing for transform functions

---
+++ testing/test_transform.py
@@ -0,0 +1,32 @@
+import pandas as pd
+import pytest
+from pipelines.transform import transform, checking_count, checking_gender
+
+@pytest.fixture
+def sample_data():
+    return pd.DataFrame({
+        'name': ['Amanda', 'sid', 'Jacob'],
+        'gender': ['F', 'm', 'x'],
+        'count': [1342, 3252, 6500]
+    })
+
+def test_transform_output(sample_data):
+    records = transform(sample_data)
+    assert isinstance(records, pd.DataFrame)
+
+def test_names_capitalized(sample_data):
+    records  = transform(sample_data)
+    assert records['names'].str.istitle().all()
+
+def test_count_validity(sample_data):
+    #todo: check for negative values
+    records = transform(sample_data)
+    assert records['count'].dtype == int
+
+def test_names_are_unique(sample_data):
+    records = transform(sample_data)
+    assert records.duplicated(subset=['name', 'gender']).sum() == 0
+
+def test_gender_values(sample_data):
+    records = transform(sample_data)
+    assert records['gender'].isin(['F', 'M']).all()
